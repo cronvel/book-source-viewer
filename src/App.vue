98dfd8c428e979b787d6d894ad18650329d7a6b3
Cédric Ronvel <cedric.ronvel@gmail.com>
2026-05-05 12:18:32 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import { ref, computed } from "vue";
-import { invoke } from "@tauri-apps/api/core";
 
+import { ref, computed } from "vue";
+import { invoke } from "@tauri-apps/api/core" ;
 // CLI plugin to retrieve args
 import { getMatches } from '@tauri-apps/plugin-cli';
-
-// FS plugin
-import { readTextFile } from '@tauri-apps/plugin-fs';
-
-// Path resolver (FS method do not accept relative path)
-import { resolve , join } from '@tauri-apps/api/path';
 
 import * as bookSource from './lib/bookSource.js' ;
 
@@ -17,36 +11,19 @@ let book_source_content = ref( "<h2>Placeholder</h2>" ) ;
 
 
 
-async function loadText( path ) {
-  const text = await ( await fetch( path ) ).text() ;
-  return text ;
-}
-
-
-
 async function loadBookSource() {
-  // Get the CWD, it's a Rust method created in src-tauri/src/lib.rs
-  const cwd = await invoke( 'get_cwd' ) ;
-  console.log( "CWD:" , cwd ) ;
+	// Get the CWD, it's a Rust method created in src-tauri/src/lib.rs
+	const cwd = await invoke( 'get_cwd' ) ;
+	console.log( "CWD:" , cwd ) ;
 
-  // Get CLI args (for dev mode, npm requires triple -- after npm in order to pass the argument to the app)
-  const matches = await getMatches() ;
-  console.log( "matches:" , matches ) ;
+	// Get CLI args (for dev mode, npm requires triple -- after npm in order to pass the argument to the app)
+	const matches = await getMatches() ;
+	console.log( "matches:" , matches ) ;
 
-  let inputPath = matches.args.input.value ;
-  console.log( "input path:" , inputPath ) ;
-  inputPath = await invoke( 'resolve_cli_path' , { input: inputPath } ) ;
-  // Resolve the path (relative pass are forbidden)
-  //inputPath = await resolve( inputPath ) ;
-  console.log( "resolved input path:" , inputPath ) ;
-  
-  const rawContent = await readTextFile( inputPath ) ;
-  console.log( "rawContent:" , rawContent ) ;
+	let inputPath = matches.args.input.value ;
+	console.log( "input path:" , inputPath ) ;
 
-  const coreCss = await loadText( '/core.css' ) ;
-  const codeCss = await loadText( '/code.css' ) ;
-
-  book_source_content.value = bookSource.bookSourceToHtml( rawContent , { coreCss, codeCss } ) ;
+	book_source_content.value = await bookSource.load( inputPath ) ;
 }
 
 loadBookSource() ;
@@ -54,127 +31,127 @@ loadBookSource() ;
 </script>
 
 <template>
-  <main class="container">
-    <div v-html="book_source_content"></div>
-  </main>
+	<main class="container">
+		<div v-html="book_source_content"></div>
+	</main>
 </template>
 
 <style>
 :root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+	font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
 
-  color: #0f0f0f;
-  background-color: #f6f6f6;
+	color: #0f0f0f;
+	background-color: #f6f6f6;
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
+	font-synthesis: none;
+	text-rendering: optimizeLegibility;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	-webkit-text-size-adjust: 100%;
 }
 
 body {
-  margin: 0;
+	margin: 0;
 }
 
 .container {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 }
 
 .book-source {
-  margin: auto;
-  /* Padding is mandatory to avoid margin collapse, e.g. on <h1> tags */
-  padding: 1em 2em;
-  max-width: 80em;
+	margin: auto;
+	/* Padding is mandatory to avoid margin collapse, e.g. on <h1> tags */
+	padding: 1em 2em;
+	max-width: 80em;
 }
 
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: 0.75s;
 }
 
 .logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
+	filter: drop-shadow(0 0 2em #24c8db);
 }
 
 .row {
-  display: flex;
-  justify-content: center;
+	display: flex;
+	justify-content: center;
 }
 
 a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
+	font-weight: 500;
+	color: #646cff;
+	text-decoration: inherit;
 }
 
 a:hover {
-  color: #535bf2;
+	color: #535bf2;
 }
 
 h1 {
-  text-align: center;
+	text-align: center;
 }
 
 input,
 button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+	border-radius: 8px;
+	border: 1px solid transparent;
+	padding: 0.6em 1.2em;
+	font-size: 1em;
+	font-weight: 500;
+	font-family: inherit;
+	color: #0f0f0f;
+	background-color: #ffffff;
+	transition: border-color 0.25s;
+	box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
 }
 
 button {
-  cursor: pointer;
+	cursor: pointer;
 }
 
 button:hover {
-  border-color: #396cd8;
+	border-color: #396cd8;
 }
 button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
+	border-color: #396cd8;
+	background-color: #e8e8e8;
 }
 
 input,
 button {
-  outline: none;
+	outline: none;
 }
 
 #greet-input {
-  margin-right: 5px;
+	margin-right: 5px;
 }
 
 @media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
+	:root {
+		color: #f6f6f6;
+		background-color: #2f2f2f;
+	}
 
-  a:hover {
-    color: #24c8db;
-  }
+	a:hover {
+		color: #24c8db;
+	}
 
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
+	input,
+	button {
+		color: #ffffff;
+		background-color: #0f0f0f98;
+	}
+	button:active {
+		background-color: #0f0f0f69;
+	}
 }
 
 </style>
--- src/lib/bookSource.js
+++ src/lib/bookSource.js
@@ -1,7 +1,45 @@
-
 import * as bookSource from 'book-source' ;
 import HtmlRenderer from 'book-source-html-renderer' ;
 import highlight from 'highlight.js' ;
+
+// Invoke Rust methods
+import { invoke } from "@tauri-apps/api/core" ;
+// FS plugin
+import { readTextFile } from '@tauri-apps/plugin-fs' ;
+// Path resolver (FS method do not accept relative path)
+//import { resolve , join } from '@tauri-apps/api/path' ;
+
+
+
+export async function load( inputPath ) {
+	inputPath = await invoke( 'resolve_cli_path' , { input: inputPath } ) ;
+
+	// Resolve the path (relative pass are forbidden)
+	//inputPath = await resolve( inputPath ) ;
+	console.log( "resolved input path:" , inputPath ) ;
+
+	let extension = getExtension( inputPath ) ;
+	console.log( "extension:" , extension ) ;
+
+	switch ( extension ) {
+		case 'bks' :
+			break ;
+		case 'kfg' :
+			break ;
+		case 'json' :
+			break ;
+		default :
+			throw new Error( "Extension '" + extension + "' not supported" ) ;
+	}
+
+	const rawContent = await readTextFile( inputPath ) ;
+	//console.log( "rawContent:" , rawContent ) ;
+
+	const coreCss = await loadText( '/core.css' ) ;
+	const codeCss = await loadText( '/code.css' ) ;
+
+	return bookSourceToHtml( rawContent , { coreCss, codeCss } ) ;
+}
 
 
 
@@ -34,4 +72,23 @@ function renderHtml( structuredDocument , params = {} ) { // , package_ ) {
 
 	return structuredDocument.render( htmlRenderer ) ;
 } ;
+
+
+
+// Utilities
+
+
+
+// Load a text file
+async function loadText( path ) {
+	const text = await ( await fetch( path ) ).text() ;
+	return text ;
+}
+
+
+
+function getExtension( filepath ) {
+	let match = filepath.match( /\.([^.]+)$/ ) ;
+	return match ? match[ 1 ].toLowerCase() : "" ;
+}
 
